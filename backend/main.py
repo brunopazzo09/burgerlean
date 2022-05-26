2d0696f1ed32e521daf126b0913d06887023660b
Bluismo <106327125+Bluismo@users.noreply.github.com>
2022-05-26 15:54:44 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,7 @@ from flask import flask, render_template
 app=flask(__name__)
 
 @app.route("/")
+dio bastardo frocio schifoso muori te e tutta la tua razza
+
 def index():
     render_template("index.html") #index
